eb7342aa8d02dd90bc86a58e065136a6358137cc
henriquetomazzz <henriqutomaz02@gmail.com>
2025-06-18 21:06:51 -0300

    implementa utilizacao da excecao IceCreamTypeNotSupportedException

--- src/main/java/com/iceCreamShop/DesignPatterns/factory/IceCreamFactory.java
+++ src/main/java/com/iceCreamShop/DesignPatterns/factory/IceCreamFactory.java
@@ -1,15 +1,16 @@
 package com.iceCreamShop.DesignPatterns.factory;
 
+import com.iceCreamShop.DesignPatterns.exception.IceCreamTypeNotSupportedException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IceCreamFactory {
-    public IceCream createIceCream(TypeIceCream type, String flavor, int quantity) {
+    public IceCream createIceCream(TypeIceCream type, String flavor, int quantity) throws IceCreamTypeNotSupportedException {
         return switch (type) {
             case SCOOP_ICE_CREAM -> new ScoopIceCream(flavor, 8.00, quantity, type);
             case POPSICLE -> new Popsicle(flavor, 5.00, quantity, type);
             case MILKSHAKE -> new Milkshake(flavor, 12.50, quantity, type);
-            default -> throw new IllegalArgumentException("Unsupported ice cream type: " + type);
+            default -> throw new IceCreamTypeNotSupportedException("Unsupported ice cream type: " + type);
         };
     }
 }
